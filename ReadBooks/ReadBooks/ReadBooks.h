3a69feccd2421c20e59cf3ad4559d90f7ef8ffa3
Aleix Diaz Baggerman <aleixbaggerman@MacBook-Pro-de-Aleix.local>
2023-08-02 14:56:25 +0200

    Create initial framework

---
+++ ReadBooks/ReadBooks/ReadBooks.h
@@ -0,0 +1,18 @@
+//
+//  ReadBooks.h
+//  ReadBooks
+//
+//  Created by Aleix Diaz Baggerman on 2/8/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReadBooks.
+FOUNDATION_EXPORT double ReadBooksVersionNumber;
+
+//! Project version string for ReadBooks.
+FOUNDATION_EXPORT const unsigned char ReadBooksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReadBooks/PublicHeader.h>
+
+
