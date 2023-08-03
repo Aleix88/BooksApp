e85da11ad602b101d21600f45aa87146ce4cef7d
Aleix Díaz <aleixdiazbaggerman@gmail.com>
2023-08-03 07:21:27 +0200

    Create Book model

---
+++ ReadBooks/ReadBooks/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  ReadBooks
+//
+//  Created by Aleix Diaz Baggerman on 3/8/23.
+//
+
+import Foundation
+
+struct Book {
+    let id: String
+    let name: String
+    let author: String
+    let imageURL: URL?
+}
