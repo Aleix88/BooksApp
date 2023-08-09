592092fc4833b1f22f1f3ac6750d98a5a06412e8
Aleix Díaz <aleixdiazbaggerman@gmail.com>
2023-08-09 07:48:57 +0200

    Inject base URL

--- ReadBooks/ReadBooksTests/BooksAPI/BookSearchURLFactoryTests.swift
+++ ReadBooks/ReadBooksTests/BooksAPI/BookSearchURLFactoryTests.swift
@@ -8,6 +8,12 @@
 import XCTest
 
 class BookSearchURLFactory {
+    let baseURL: URL
+    
+    init(baseURL: URL) {
+        self.baseURL = baseURL
+    }
+    
     func create(input: String) -> URL? {
         return nil
     }
@@ -16,7 +22,7 @@ class BookSearchURLFactory {
 final class BookSearchURLFactoryTests: XCTestCase {
 
     func test_create_returnsNilWithInvalidInput() {
-        let sut = BookSearchURLFactory()
+        let sut = BookSearchURLFactory(baseURL: URL(string: "https://www.somebaseurl.com")!)
         XCTAssertNil(sut.create(input: ""))
         XCTAssertNil(sut.create(input: "    "))
         XCTAssertNil(sut.create(input: "\n\n\n"))
