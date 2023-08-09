744990b9fd36878c9452e0e130b5d77a41d029ca
Aleix Díaz <aleixdiazbaggerman@gmail.com>
2023-08-09 08:43:58 +0200

    Create end to end tests target

---
+++ ReadBooks/ReadBooksEndToEndTests/ReadBooksEndToEndTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReadBooksEndToEndTests.swift
+//  ReadBooksEndToEndTests
+//
+//  Created by Aleix Diaz Baggerman on 9/8/23.
+//
+
+import XCTest
+
+final class ReadBooksEndToEndTests: XCTestCase {
+
+    func test() {
+        
+    }
+
+}
