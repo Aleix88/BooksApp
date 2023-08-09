7d6c8d654ede803f1dfd9b57eb5fb52fb84b252d
Aleix Díaz <aleixdiazbaggerman@gmail.com>
2023-08-09 07:45:48 +0200

    Create BookSearchURLFactory and add invalid input test

---
+++ ReadBooks/ReadBooksTests/BooksAPI/BookSearchURLFactoryTests.swift
@@ -0,0 +1,23 @@
+//
+//  BookSearchURLFactory.swift
+//  ReadBooksTests
+//
+//  Created by Aleix Diaz Baggerman on 9/8/23.
+//
+
+import XCTest
+
+class BookSearchURLFactory {
+    func create(input: String) -> URL? {
+        return nil
+    }
+}
+
+final class BookSearchURLFactoryTests: XCTestCase {
+
+    func test_create_returnsNilWithInvalidInput() {
+        let sut = BookSearchURLFactory()
+        XCTAssertNil(sut.create(input: ""))
+    }
+
+}
